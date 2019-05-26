8488502c1e17331ee3a0ae89f55e1d517a13fd2a
Orugames <asm.softdeveloper@gmail.com>
2019-05-26 17:48:35 +0200

    Initial Stages of level Editor

---
+++ Assets/Scripts/GridCubeLogic.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[ExecuteInEditMode]
+public class GridCubeLogic : MonoBehaviour
+{
+    public TextMeshPro text;
+
+    public int x;
+    public int y;
+    public bool init;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (init) Initialize();
+    }
+
+    public  void Initialize()
+    {
+        init = false;
+        x = (int)transform.position.x;
+        y = (int)transform.position.y;
+        text.text = x.ToString() + "," + y.ToString();
+        name = text.text;
+    }
+}
---
+++ Assets/Scripts/LevelEditor.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class LevelEditor : MonoBehaviour
+{
+    float gridSize = 1;
+
+    public  GameObject prefab;
+    public bool modifyValues;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void OnMouseDown()
+    {
+      
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                Vector3 point2 = hit.point;
+                GameObject newPrefab = Instantiate(prefab);
+
+                Vector3 rounded;
+                rounded.x = Mathf.RoundToInt((hit.point.x * gridSize) / gridSize);
+                rounded.y = Mathf.RoundToInt((hit.point.y * gridSize) / gridSize);
+                rounded.z = 0;
+                newPrefab.transform.position = rounded;
+                newPrefab.GetComponent<GridCubeLogic>().Initialize();
+            }
+        
+
+    }
+
+    
+
+}
