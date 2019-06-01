a4fe3f31f7f440c1dc164b640c884954e9ed9ff5
Orugames <asm.softdeveloper@gmail.com>
2019-06-01 20:02:35 +0200

    Updated further the delete Levels button. Now if the button is the last one also works properly.

--- Assets/Scripts/Lobby/LobbyLogic.cs
+++ Assets/Scripts/Lobby/LobbyLogic.cs
@@ -27,7 +27,7 @@ public class LobbyLogic : MonoBehaviour
         {
             ES3.Save<int>("MaxLevelsCreated", maxLevels = 0);
         }
-        
+        Debug.Log("Maxlevels" + maxLevels);
         for (int i = 0; i < maxLevels; i++)
         {
             GameObject levelSelectionButton = Instantiate(buttonPrefab);
@@ -43,7 +43,7 @@ public class LobbyLogic : MonoBehaviour
             int x;
             x = i+1;
             
-            deleteButton.onClick.AddListener(delegate () { DestroyLevel(x.ToString()); }); //BUG of delegates
+            deleteButton.onClick.AddListener(delegate () { UpdateSaveLevels(x); }); //BUG of delegates
 
             b.onClick.AddListener(delegate () { LoadCreatedLevel(x); });
 
@@ -92,17 +92,124 @@ public class LobbyLogic : MonoBehaviour
         ES3.Save<int>("MaxLevelsCreated",maxLevels);
 
         GameObject buttonToRemoveGO = levelsButtons[levelNumber];
-        levelsButtons.Remove(levelsButtons[levelNumber]);
+  
+        
+    }
+    public void UpdateSaveLevels(int LevelToDelete)
+    {
+
+        Debug.Log("Level to delete " + LevelToDelete);
+        int maxLevels = 0;
+        if (ES3.KeyExists("MaxLevelsCreated"))
+        {
+            maxLevels = ES3.Load<int>("MaxLevelsCreated");
+        }
+        else
+        {
+            ES3.Save<int>("MaxLevelsCreated", maxLevels = 0);
+        }
+
+        int nCubes;
+        GameObject buttonToRemoveGO;
+        if (LevelToDelete == maxLevels)
+        {
+            Debug.Log("Nivel elegido para borrar es el ultimo");
+
+            DeleteEndOfSavedLevels(maxLevels);
+
+            buttonToRemoveGO = levelsButtons[maxLevels-1];
+            levelsButtons.Remove(levelsButtons[maxLevels-1]);
+            levelsButtons.TrimExcess();
+            Destroy(buttonToRemoveGO);
+
+            foreach (Transform child in gridParent.transform)
+            {
+                Destroy(child.gameObject);
+            }
+            Start();
+            return;
+        }
+
+        Debug.Log("Nivel elegido para borrar no es el ultimo, se actualiza la lista");
+        for (int k = LevelToDelete; k < maxLevels; k++)
+        {
+            nCubes = ES3.Load<int>("NumberCubesLevel" + (k+1));
+            ES3.Save<int>("NumberCubesLevel" + k, nCubes);
+
+            for (int i = 0; i < nCubes; i++)
+            {
+                int prefabNum = ES3.Load<int>("Cube" + i + "Level" + (k + 1) + "Prefab");
+                ES3.Save<int>("Cube" + i + "Level" + k + "Prefab", prefabNum);
+
+                Cube newCube = ES3.Load<Cube>("Cube" + i + "Level" + (k + 1));
+                ES3.Save<Cube>("Cube" + i + "Level" + k, newCube);
+
+                for (int j = 0; j < 6; j++)
+                {
+
+                    CubeSide newCubeSide = ES3.Load<CubeSide>("CubeSide" + j + "Cube" + i + "Level" + (k + 1));
+                    ES3.Save<CubeSide>("CubeSide" + j + "Cube" + i + "Level" + k, newCubeSide);
+                }
+            }
+
+        }
+        //Method to trim the end of the saveGames
+        DeleteEndOfSavedLevels(maxLevels);
+
+        buttonToRemoveGO = levelsButtons[LevelToDelete];
+        levelsButtons.Remove(levelsButtons[LevelToDelete]);
         levelsButtons.TrimExcess();
         Destroy(buttonToRemoveGO);
-        
+
+        foreach(Transform child in gridParent.transform)
+        {
+            Destroy(child.gameObject);
+        }
+        Start();
+
+        /*int nCubes = ES3.Load<int>("NumberCubesLevel" + 3 + 1);
+        ES3.Save<int>("NumberCubesLevel" + 3, nCubes);
+
+        for (int i = 0; i < nCubes; i++)
+        {
+            int prefabNum = ES3.Load<int>("Cube" + i + "Level" + 3 + 1 + "Prefab");
+            ES3.Save<int>("Cube" + i + "Level" + 3 + "Prefab", prefabNum);
+
+            Cube newCube = ES3.Load<Cube>("Cube" + i + "Level" + 3 + 1);
+            ES3.Save<Cube>("Cube" + i + "Level" + 3, newCube);
+
+            for (int j = 0; j < 6; j++)
+            {
+
+                CubeSide newCubeSide = ES3.Load<CubeSide>("CubeSide" + j + "Cube" + i + "Level" + 3 + 1);
+                ES3.Save<CubeSide>("CubeSide" + j + "Cube" + i + "Level" + 3, newCubeSide);
+            }
+        }*/
+
+
     }
 
 
-    // Update is called once per frame
-    void Update()
+    void DeleteEndOfSavedLevels(int maxLevels)
     {
-        
+        int nCubes = 0;
+        nCubes = ES3.Load<int>("NumberCubesLevel" + maxLevels);
+        for (int i = 0; i < nCubes; i++)
+        {
+            ES3.DeleteKey("Cube" + i + "Level" + maxLevels);
+            ES3.DeleteKey("Cube" + i + "Level" + maxLevels + "Prefab");
+
+            for (int j = 0; j < 6; j++)
+            {
+                ES3.DeleteKey("CubeSide" + j + "Cube" + i + "Level" + maxLevels);
+
+            }
+        }
+        ES3.DeleteKey("NumberCubesLevel" + maxLevels);
+        int maxLevelsNew = ES3.Load<int>("MaxLevelsCreated");
+        maxLevelsNew -= 1;
+        ES3.Save<int>("MaxLevelsCreated", maxLevelsNew);
+
     }
-   
+
 }
