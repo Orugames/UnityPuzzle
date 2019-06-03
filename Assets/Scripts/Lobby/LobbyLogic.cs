c7753f1ebaade0d38dbef153b0b5ca730eab6f1e
Orugames <asm.softdeveloper@gmail.com>
2019-06-04 00:20:00 +0200

    Updated the model of the sideCube. Next commit will add new logic to the colors of the sides.

--- Assets/Scripts/Lobby/LobbyLogic.cs
+++ Assets/Scripts/Lobby/LobbyLogic.cs
@@ -130,6 +130,7 @@ public class LobbyLogic : MonoBehaviour
 
         int nCubes;
         GameObject buttonToRemoveGO;
+
         if (LevelToDelete == maxLevels) //if we destroy the last level on the list
         {
             Debug.Log("Nivel elegido para borrar es el ultimo");
