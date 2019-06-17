6f7f46957f1de689bd4eb87f2a536e2c806943db
Orugames <asm.softdeveloper@gmail.com>
2019-06-17 21:59:24 +0200

    updated the ui of level selection

---
+++ Assets/Scripts/Lobby/ExpandShrinkLevels.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class ExpandShrinkLevels : MonoBehaviour
+{
+    public LayoutElement layoutElement;
+    public bool expanded;
+    public bool finishedAnim = true;
+    public bool smallList;
+
+    void Start()
+    {
+        
+    }
+    public void MoveButtons(bool movement)
+    {
+        
+        if (movement && finishedAnim)
+        {
+            finishedAnim = false;
+            if (smallList) layoutElement.DOPreferredSize(new Vector2(570, 200), 0.5f).OnComplete(() =>{
+                finishedAnim = true;
+                expanded = true;
+            });
+            else layoutElement.DOPreferredSize(new Vector2(570, 400), 0.5f).OnComplete(() => {
+                finishedAnim = true;
+                expanded = true;
+            });
+        }
+        else if (!movement && finishedAnim)
+        {
+            finishedAnim = false;
+            layoutElement.DOPreferredSize(new Vector2(570, 0), 0.5f).OnComplete(() => {
+                finishedAnim = true;
+                expanded = false;
+            }); ;
+        }
+    }
+    // Update is called once per frame
+    /*public IEnumerator LerpLayout(bool expand)
+    {
+        layoutElement.DOPreferredSize(new Vector2(570,400), 2);
+        layoutElement.preferredHeight = 200 * lerpValue;
+        yield return new WaitForEndOfFrame();
+    }*/
+}
