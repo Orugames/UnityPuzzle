95e1172bbd48407309618754c20b511c2b1aa5a8
Orugames <asm.softdeveloper@gmail.com>
2019-06-18 22:09:57 +0200

    Changed the looks of the game

--- Assets/Scripts/Lobby/LobbyUIController.cs
+++ Assets/Scripts/Lobby/LobbyUIController.cs
@@ -8,16 +8,22 @@ public class LobbyUIController : MonoBehaviour
     public RectTransform mainMenuPanel;
     public RectTransform LevelSelectionPanel;
 
+
+    public RectTransform orangeTransition;
+    public RectTransform blackTransition;
+
+
+
     public bool tweenFinished = true;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void MovePanels(int selection)
     {
@@ -28,10 +34,17 @@ public class LobbyUIController : MonoBehaviour
             case 0:
                 mainMenuPanel.DOAnchorPos(new Vector2(0, 0), 0.5f).OnComplete(() => tweenFinished = true);
                 LevelSelectionPanel.DOAnchorPos(new Vector2(800, 0), 0.5f);
+                blackTransition.DOScale(0, 0.4f);
+                orangeTransition.DOScale(0, 0.4f).SetDelay(0.1f);
                 break;
             case 1:
                 mainMenuPanel.DOAnchorPos(new Vector2(-800, 0), 0.5f);
-                LevelSelectionPanel.DOAnchorPos(new Vector2(0, 0), 0.5f).OnComplete(() => tweenFinished = true); ;
+                blackTransition.DOScale(20, 1f).SetDelay(0.2f);
+                orangeTransition.DOScale(20, 1).OnComplete(() =>
+                {
+                    LevelSelectionPanel.DOAnchorPos(new Vector2(0, 0), 0.5f).OnComplete(() => tweenFinished = true);
+
+                });
                 break;
         }
     }
